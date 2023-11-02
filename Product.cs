5c60628f0d2b021fac85bb431368d0618953145c
u31d79 <u31d79@INGURWN110491>
2023-11-02 22:52:13 +0530

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    internal class Product
+    {
+        readonly int pCode;
+        string pName;
+        int qty_in_stock;
+        int discount_allowed;
+        static string brand;
+        public Product() { }
+        public Product(int pCode,
+        string pName,
+        int qty_in_stock,
+        int discount_allowed)
+        {
+            this.pCode = pCode;
+            this.pName = pName;
+            this.qty_in_stock = qty_in_stock;
+            this.discount_allowed = discount_allowed;
+        }
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Enter Product Name");
+            pName = Console.ReadLine();
+            Console.WriteLine("ENter Discoubt");
+            discount_allowed = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("ENter qty_in_stock");
+            qty_in_stock = Byte.Parse(Console.ReadLine());
+
+        }
+        public Product(string Pcode, Product product)
+        {
+            this.pCode = pCode;
+            this.pName = product.pName;
+            this.discount_allowed = product.discount_allowed;
+            this.qty_in_stock = product.qty_in_stock;
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine("Product code " + pCode);
+            Console.WriteLine(pName);
+            Console.WriteLine(qty_in_stock);
+            Console.WriteLine(discount_allowed);
+
+        }
+
+        static Product()
+        {
+
+            brand = "Polo";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Assignment_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            string c = "y";
+            while (c == "y")
+            {
+                Console.WriteLine("Who are you");
+                string userType = Console.ReadLine();
+                switch (userType.ToLower())
+                {
+                    case "admin":
+                        Console.WriteLine("What to do");
+                        int ch = Byte.Parse(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case 1:
+                                {
+                                    string choice = "y";
+                                    while (choice == "y")
+                                    {
+                                        Console.WriteLine("Enter P Code");
+                                        string pCode = Console.ReadLine();
+                                        Product product = new Product();
+                                        product.GetProductDetails();
+                                        Product temp = new Product(pCode, product);
+                                        products.Add(temp);
+                                        Console.WriteLine("Add more record");
+                                        choice = Console.ReadLine();
+                                    }
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    foreach (Product product in products)
+                                    {
+                                        product.DisplayProducts();
+                                    }
+                                    break;
+                                }
+                        }
+                        break;
+                    case "customer":
+                        break;
+                }
+            }
+            Console.WriteLine("Want to repeat");
+            c = Console.ReadLine();
+        }
+    }
+}
